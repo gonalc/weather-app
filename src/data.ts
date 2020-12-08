0f7a9ebd2bc3cc4732e7f7981552d2cd0cc4fd74
Gonzalo <gonalc8@gmail.com>
2020-12-08 10:11:10 +0100

    data helper

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,16 @@
+import axios from 'axios';
+import moment from 'moment';
+const apiKey = process.env.VUE_APP_API_KEY;
 // Helper functions to use across the app
-
 export const fromKelvinToCelsius = (temp: number) => (temp - 273.15).toFixed(1);
+export const getHistoricalWeather = async (lat: number, lon: number) => {
+    try {
+        const start = moment().subtract(30, 'days').format('X');
+        const end = moment().subtract(1, 'days').format('X');
+        const url = `http://history.openweathermap.org/data/2.5/history/city?lat=${lat}&lon${lon}&type=hour&start=${start}&end=${end}&appid=${apiKey}`;
+        const historical = await axios.get(url);
+        console.log({ historical });
+    } catch (err) {
+        console.error('Error fetching historical data: ', err);
+    }
+};
